e8b34b3b87e4d21b914bb8ba907ebd1b708f43af
Xyspace Network <network.xyspace@gmail.com>
2025-04-20 18:28:34 +0530

    Create App.tsx

---
+++ SellerApp/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import RootComponent from './RootComponent'
+import { persistor, store } from './store/reducers/store'
+
+import { Button } from 'antd'
+
+const App: React.FC = () => {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <RootComponent />
+            </PersistGate>
+        </Provider>
+    )
+}
+
+export default App
